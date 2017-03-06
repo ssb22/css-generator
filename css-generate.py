e197a255d2ba1e95ee457d5d1ea4d368c25300e5
Silas S. Brown <ssb22@cam.ac.uk>
2017-03-06 17:00:55 +0000

    Update css-generate.py
    git-svn-id: http://svn.code.sf.net/p/e-guidedog/code/ssb22/css-generator@2732 29193198-4895-4776-b068-10539e920549

--- css-generate.py
+++ css-generate.py
@@ -832,8 +832,8 @@ def do_one_stylesheet(pixelSize,colour,filename,debugStopAfter=0):
         "position":"fixed","top":"0px","left":"auto",
         "right":"0px", # right, not left, or overflow problems, + right helps w. tooltips
         "width":"30%", # not fixed+100% or PgDn will go wrong
-        "max-width":"30%", # how are they managing to make it multi-column ??!?
         "overflow":"auto","border":"blue solid","z-index":"1"}
+      css["body > input#site + div#wrapper > div#regionHeader div"]={"display":"block"} # not inline-block: vertical scrolling please, not horizontal: we've changed the layout
       css[jumpjsContent]={"margin-right":"31%","z-index":"0"} # to match the 30% (i.e. take 70%, actually 69%)
       css[jjc+"div#secondaryNavContent"]={"*display":"block"} # not None, even if the screen SEEMS to be too small, because we've changed the layout
       css[jjc + "div#secondaryNav"]={"position":"fixed", # or double-scroll JS fails
