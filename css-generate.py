f2973f056e6d9baea5443465fa39e6afcf10240d
Silas S. Brown <ssb22@cam.ac.uk>
2018-04-10 08:02:24 +0100

    update

--- css-generate.py
+++ css-generate.py
@@ -1148,6 +1148,7 @@ def do_one_stylesheet(pixelSize,colour,filename,debugStopAfter=0):
     "*display":"block", # not flex
     "*max-width":"100%"}
   css['div.tabs > ul.tabs-header-container > li.tabheader.active > a h2, div.tabs > ul.tabs-header-container > li.tabheader.active > a span']={'color':colour["text"]} # Tesco 2018-02 again (confusing non-functional link as current tab)
+  css['ul.product-list div.inputControl-wrapper']={'*display':'block'} # not flex, Tesco 2018-04
 
   css['body.page-template header.site-header + div.all-site-wrap > div.page-wrap + aside.entry-unrelated']={'*display':'none'} # sorry css-tricks but it was making the article unreadable
   
