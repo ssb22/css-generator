0bd54e12669577c898176e0a29b0fff8b30173b0
Silas S. Brown <ssb22@cam.ac.uk>
2019-01-26 19:22:16 +0000

    update

--- css-generate.py
+++ css-generate.py
@@ -1537,7 +1537,7 @@ var defaultSize=35; if(screen && screen.height) defaultSize=screen.height/(windo
   def tryStylesheetJS(hrefExpr):
     r = "var e=document.createElement('link');e.id0='ssb22css';e.rel='stylesheet';e.href="+hrefExpr+";if(!document.getElementsByTagName('head'))document.body.appendChild(document.createElement('head'));var h=document.getElementsByTagName('head')[0];if(h.lastChild&&h.lastChild.id0=='ssb22css')h.removeChild(h.lastChild);h.appendChild(e);" # try to avoid spaces because they get written as %20 in the bookmarklet
     if alternate_server_for_https_requests:
-      r = "var c="+hrefExpr+","+r[4:].replace(hrefExpr,"location.protocol=='https:'?'"+alternate_server_for_https_requests+r"'+c.slice(c.search(/[^/]*\\.css/)).replace('.css',''):c",1)
+      r = "var c="+hrefExpr+","+r[4:].replace(hrefExpr,"location.protocol=='https:'?'"+alternate_server_for_https_requests+r"'+c.slice(c.search(/[^/]*\\.css/)).replace(/[.]css.*/,''):c",1)
     return r
   # (do NOT put that in a JS function, the 1st link must be self-contained.  and don't say link.click() it's too browser-specific)
   if alternate_server_for_https_requests: exception = ""
