7d7d49c59b1db43ec2a8d0160d6f42cd55cbb4f9
Silas S. Brown <ssb22@cam.ac.uk>
2019-11-30 06:28:36 +0000

    update

--- css-generate.py
+++ css-generate.py
@@ -836,11 +836,11 @@ def do_one_stylesheet(pixelSize,colour,filename,debugStopAfter=0):
   css['div.highlight > pre span.n,div.highlight > pre span.nn']=shl_varname
   css['div.highlight > pre span.s1,div.highlight > pre span.s2,div.highlight > pre span.sd,div.highlight > pre span.si']=shl_string
   # TODO: p = punc (and do we differentiate sd=docstring, si=% formatter)
-  css['pre > code.hljs > span.hljs-keyword'] = shl_keyword
-  css['pre > code.hljs > span.hljs-built_in'] = shl_varname
-  css['pre > code.hljs > span.hljs-string'] = shl_string
-  css['pre > code.hljs > span.hljs-comment'] = shl_comment
-  css['pre > code.hljs > span.hljs-number'] = shl_preproc
+  css['pre > code.hljs span.hljs-keyword'] = shl_keyword
+  css['pre > code.hljs span.hljs-built_in'] = shl_varname
+  css['pre > code.hljs span.hljs-string'] = shl_string
+  css['pre > code.hljs span.hljs-comment'] = shl_comment
+  css['pre > code.hljs span.hljs-number'] = shl_preproc
   css['pre.code > span.com'] = shl_comment
   css['pre.code > span.str'] = shl_string
   css['pre.code > span.kwd'] = shl_keyword
