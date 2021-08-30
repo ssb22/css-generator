d3a6ce52241e4b018ccf917830611ed02cd236cf
Silas S. Brown <ssb22@cam.ac.uk>
2021-08-30 11:27:20 +0100

    GitLab merge-request status visibility

--- css-generate.py
+++ css-generate.py
@@ -1233,6 +1233,7 @@ def do_one_stylesheet(pixelSize,colour,filename,debugStopAfter=0):
   css["div.issues-other-filters div.dropdown button.dropdown-menu-toggle span.dropdown-toggle-text svg,a#logo span.logo-text svg,body.ui-indigo div.nav-sidebar a div.nav-icon-container svg,body.ui-indigo a.toggle-sidebar-button svg,body.ui-indigo div.breadcrumbs-links svg.breadcrumbs-list-angle,body.ui-indigo svg.caret-down"]={"*display":"none"} # GitLab 2018
   css["a.note-emoji-button > svg.s16, button > svg.s16"]={"*height":"16px","*width":"16px"} # GitLab 2019
   css["form.new-note div.md-area a.zen-control"]={"*display":"none"} # GitLab 2019
+  css["div.merge-request div.status-box svg + span.gl-display-none"]={"display":"inline","border":"purple solid"} # GitLab 2021 (be clearer about Open / Merged, not just a small SVG icon)
   css['#calendar td.fc-widget-content.day-available']={'border':'green solid'}
   css["div.issues-listing div.state.state-closed"]={'color':colour["coloured"],'border':'red solid','font-weight':'bold'} # GitHub: make it slightly more obvious if we're looking at a closed ticket
   css["summary > svg.icon-chevon-down-mktg, details > summary > svg[viewBox=\"0 0 14 8\"]"]={'*display':'none'} # sorry GitHub, it's way too big when in giant print (and don't know why doHeightWidth isn't fixing these)
