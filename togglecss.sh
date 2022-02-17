94407c6e0f80d8a26be4e6c7f45827faab27c9ca
Silas S. Brown <ssb22@cam.ac.uk>
2022-02-17 09:44:41 +0000

    remove unnecessary exports

--- togglecss.sh
+++ togglecss.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Toggle user stylesheet and restart Firefox
-# Silas S. Brown 2011,2021, public domain, no warranty
+# Silas S. Brown 2011,2021-22, public domain, no warranty
 
 # Where to find history:
 # on GitHub at https://github.com/ssb22/css-generator
@@ -14,15 +14,15 @@ if ! cd "$HOME/.mozilla/firefox"; then
   echo "Could not find Firefox profiles directory"
   exit 1
 fi
-export Done=0
+Done=0
 for D in *.default; do
   cd "$D/chrome" || continue
   if test -e userContent.css; then
     mv userContent.css userContent.css-disabled
-    export Done=1
+    Done=1
   elif test -e userContent.css-disabled; then
     mv userContent.css-disabled userContent.css
-    export Done=1
+    Done=1
   fi
   cd ../..
 done
