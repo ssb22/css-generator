8b4ec5d78ab9d53113e7f3e74c553aba9043ba92
Silas S. Brown <ssb22@cam.ac.uk>
2023-11-09 08:16:09 +0000

    fix GitLab merge-request checkboxes at size=unchanged

--- css-generate.py
+++ css-generate.py
@@ -1506,6 +1506,7 @@ def do_one_stylesheet(pixelSize,colour,filename,debugStopAfter=0):
 
   css['div#pt_checkout_onepage input[type="checkbox"],div#pt_checkout_onepage input[type="radio"],body#SurveyEngineBody input[type="checkbox"],body#SurveyEngineBody input[type="radio"]']={'opacity':'1','position':'static'} # Claires checkout junk-signup checkbox + Qualtrics surveys: please make current state visible (don't just use colours that might be overridden or not seen)
   css['input.oo-ui-inputWidget-input[type="checkbox"]']={'opacity':'1'} # e.g. MediaWiki on Wenlin edit pages: please make current state more visible
+  css['div.gl-display-flex input[type="checkbox"]']={'opacity':'1'} # GitLab had the same problem
 
   css['span.starRating-blank:empty:before']={'opacity':'0.5'} # Yellow Pages
 
