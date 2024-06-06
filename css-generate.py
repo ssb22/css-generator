24e8bbeb3ded14668b115a93a2c3a068d6af8756
Silas S. Brown <ssb22@cam.ac.uk>
2024-06-06 12:52:47 +0100

    remove fix for Wattpad private messaging because that service has been shut down

--- css-generate.py
+++ css-generate.py
@@ -1624,7 +1624,6 @@ def do_one_stylesheet(pixelSize,colour,filename,debugStopAfter=0):
   css['section#l-main-content > div.l-ad,section#l-main-content > div.l-ad div']={'**background':'transparent'} # Business Insider
   css['html.bc-iframe video-js div,div[data-pagelet="WatchPermalinkVideo"] div']={'**background':'transparent'}
   css['body > div.cdk-overlay-container']={'**background':'transparent'} # AngularJS
-  css['div.from-other div.conversation-message']={'border':'green solid'}
 
   # End site-specific hacks
   css[":root:not(html) svg *"]={"color":colour["text"],"background":colour["background"]} # needed for some UI controls on Firefox 62
