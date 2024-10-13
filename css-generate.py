a97eff73068e352c89f224721e6f91e53f96287a
Silas S. Brown <ssb22@cam.ac.uk>
2024-10-13 07:10:44 +0100

    fix MS SharePoint PDF viewer

--- css-generate.py
+++ css-generate.py
@@ -1168,6 +1168,8 @@ def do_one_stylesheet(pixelSize,colour,filename,debugStopAfter=0):
   css['div#player.ytd-watch-flexy div']={'**background':'transparent'} # YouTube itself 2024 (but seek-previews still don't show)
   css['div#content.ytd-app div']={'**background':'transparent'} # YouTube 'shorts'
   css['div.OnePlayer-container div']={'**background':'transparent'}
+  css['div[id^="ModalFocusTrapZone"],div[class^="OneUpViewer"] div,div.ms-Overlay']={'**background':'transparent'}
+  if not colour["text"]=="black": css['div[class^="pdfContent_"] canvas']={'background':'white','filter':'invert(1)'}
   emptyLink("div.welcome-wrapper > nav > div.container > div.navbar-header > button.navbar-toggle > span:first-child","Toggle navigation",css,printOverride,colour,False)
   emptyLink("div.btn-group > button#hideNames > i.fa-eye-slash","Hide names",css,printOverride,colour,False)
   emptyLink("div.btn-group > button#showNames > i.fa-eye","Show names",css,printOverride,colour,False)
